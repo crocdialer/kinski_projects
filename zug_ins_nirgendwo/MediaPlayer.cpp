2ecfb9bad49f59d85c15626525428b762dea8a14
crocdialer <crocdialer@googlemail.com>
2016-04-22 12:14:18 +0200

    label placement

--- zug_ins_nirgendwo/MediaPlayer.cpp
+++ zug_ins_nirgendwo/MediaPlayer.cpp
@@ -97,7 +97,7 @@ void MediaPlayer::update(float timeDelta)
 void MediaPlayer::draw()
 {
     // background text
-    gl::draw_text_2D(name(), fonts()[1], gl::COLOR_WHITE, vec2(55, 55));
+    gl::draw_text_2D(name(), fonts()[1], gl::COLOR_WHITE, vec2(75, 55));
     
     if(!m_movie_start_timers.empty() && !m_movie_start_timers[0].has_expired())
     {
