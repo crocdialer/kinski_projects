79a9912952bcd198e2049aea5940422da8106a61
crocdialer <crocdialer@googlemail.com>
2016-06-06 14:47:28 +0200

    added modified media_player for kazi

---
+++ kazi_player/MediaPlayer.hpp
@@ -0,0 +1,91 @@
+// __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+//
+// Copyright (C) 2012-2016, Fabian Schmidt <crocdialer@googlemail.com>
+//
+// It is distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt
+// __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+
+//  MediaPlayer.hpp
+//
+//  Created by Fabian on 29/01/14.
+
+#pragma once
+
+#include "app/ViewerApp.hpp"
+#include "app/WarpComponent.hpp"
+#include "gl/Texture.hpp"
+
+#include "media/media.h"
+
+namespace kinski
+{
+    class MediaPlayer : public ViewerApp
+    {
+    private:
+
+        enum TextureEnum{TEXTURE_INPUT = 0, TEXTURE_OUTPUT = 1};
+
+        WarpComponent::Ptr m_warp;
+
+        media::MediaControllerPtr m_movie = media::MovieController::create();
+        media::CameraControllerPtr m_camera_control = media::CameraController::create();
+        bool m_reload_movie = false;
+        Timer m_broadcast_timer;
+        
+        // properties
+        Property_<string>::Ptr m_movie_path = Property_<string>::create("movie path", "");
+        Property_<bool>::Ptr
+        m_loop = Property_<bool>::create("loop", false),
+        m_auto_play = Property_<bool>::create("autoplay", true),
+        m_use_warping = Property_<bool>::create("use warping", true),
+        m_force_audio_jack = Property_<bool>::create("force 3.5mm audio-jack", false),
+        m_use_discovery_broadcast = Property_<bool>::create("use discovery broadcast", true);
+        
+        Property_<float>::Ptr
+        m_playback_speed = Property_<float>::create("playback speed", 1.f),
+        m_volume = RangedProperty<float>::create("volume", 1.f, 0.f , 1.f),
+        m_stream_switch_interval = Property_<float>::create("switch stream every x secs", 75.f);
+        
+        Property_<uint32_t>::Ptr
+        m_broadcast_port = Property_<uint32_t>::create("discovery broadcast port", 55555);
+        
+        Property_<std::vector<string>>::Ptr
+        m_stream_urls = Property_<std::vector<string>>::create("stream urls");
+        
+        Timer m_stream_timer;
+        
+        int m_stream_current_index = -1;
+        
+        std::string secs_to_time_str(float the_secs) const;
+        void setup_rpc_interface();
+        
+        void reload_movie();
+        
+    public:
+
+        MediaPlayer(int argc = 0, char *argv[] = nullptr):ViewerApp(argc, argv){};
+        void setup() override;
+        void update(float timeDelta) override;
+        void draw() override;
+        void resize(int w ,int h) override;
+        void keyPress(const KeyEvent &e) override;
+        void keyRelease(const KeyEvent &e) override;
+        void mousePress(const MouseEvent &e) override;
+        void mouseRelease(const MouseEvent &e) override;
+        void mouseMove(const MouseEvent &e) override;
+        void mouseDrag(const MouseEvent &e) override;
+        void mouseWheel(const MouseEvent &e) override;
+        void touch_begin(const MouseEvent &e, const std::set<const Touch*> &the_touches) override;
+        void touch_end(const MouseEvent &e, const std::set<const Touch*> &the_touches) override;
+        void touch_move(const MouseEvent &e, const std::set<const Touch*> &the_touches) override;
+        void fileDrop(const MouseEvent &e, const std::vector<std::string> &files) override;
+        void tearDown() override;
+        void update_property(const Property::ConstPtr &theProperty) override;
+        
+        bool save_settings(const std::string &path = "") override;
+        bool load_settings(const std::string &path = "") override;
+
+    };
+}// namespace kinski
