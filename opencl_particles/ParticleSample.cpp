55bcde7ffae5a8b07b5c749621433f840d0d2386
crocdialer <crocdialer@googlemail.com>
2017-04-24 21:00:06 +0200

    added new particle sample

---
+++ opencl_particles/ParticleSample.cpp
@@ -0,0 +1,196 @@
+//
+//  ParticleSample.cpp
+//  gl
+//
+//  Created by Fabian on 29/01/14.
+//
+//
+
+#include "ParticleSample.hpp"
+
+using namespace std;
+using namespace kinski;
+using namespace glm;
+
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::setup()
+{
+    ViewerApp::setup();
+    m_particle_system.opencl().init();
+    register_property(m_texture_path);
+    register_property(m_num_particles);
+    register_property(m_point_size);
+    register_property(m_point_color);
+    register_property(m_start_velocity_min);
+    register_property(m_start_velocity_max);
+    register_property(m_lifetime_min);
+    register_property(m_lifetime_max);
+    observe_properties();
+    add_tweakbar_for_component(shared_from_this());
+    load_settings();
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::init_particles(uint32_t the_num)
+{
+    auto geom = gl::Geometry::create();
+    auto mat = gl::Material::create(gl::ShaderType::POINTS_COLOR);
+    geom->set_primitive_type(GL_POINTS);
+    m_particle_mesh = gl::Mesh::create(geom, mat);
+
+    geom->vertices().resize(the_num, vec3(0));
+    geom->colors().resize(the_num, gl::COLOR_WHITE);
+    geom->point_sizes().resize(the_num, *m_point_size);
+
+    float vals[2];
+    glGetFloatv(GL_POINT_SIZE_RANGE, vals);
+    m_point_size->set_range(vals[0], vals[1]);
+    mat->set_point_size(*m_point_size);
+    mat->set_point_attenuation(0.f, 0.01f, 0.f);
+    mat->uniform("u_pointRadius", 50.f);
+    mat->set_point_size(1.f);
+    mat->set_blending();
+
+    m_particle_system.set_mesh(m_particle_mesh);
+
+    m_needs_init = false;
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::update(float timeDelta)
+{
+    ViewerApp::update(timeDelta);
+
+    if(m_needs_init){ init_particles(*m_num_particles); }
+    m_particle_system.update(timeDelta);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::draw()
+{
+    gl::set_matrices(camera());
+    if(*m_draw_grid){ gl::draw_grid(50, 50); }
+
+    scene()->render(camera());
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::resize(int w ,int h)
+{
+    ViewerApp::resize(w, h);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::key_press(const KeyEvent &e)
+{
+    ViewerApp::key_press(e);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::key_release(const KeyEvent &e)
+{
+    ViewerApp::key_release(e);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::mouse_press(const MouseEvent &e)
+{
+    ViewerApp::mouse_press(e);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::mouse_release(const MouseEvent &e)
+{
+    ViewerApp::mouse_release(e);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::mouse_move(const MouseEvent &e)
+{
+    ViewerApp::mouse_move(e);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::touch_begin(const MouseEvent &e, const std::set<const Touch*> &the_touches)
+{
+
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::touch_end(const MouseEvent &e, const std::set<const Touch*> &the_touches)
+{
+
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::touch_move(const MouseEvent &e, const std::set<const Touch*> &the_touches)
+{
+
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::mouse_drag(const MouseEvent &e)
+{
+    ViewerApp::mouse_drag(e);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::mouse_wheel(const MouseEvent &e)
+{
+    ViewerApp::mouse_wheel(e);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::file_drop(const MouseEvent &e, const std::vector<std::string> &files)
+{
+    for(const string &f : files){ LOG_INFO << f; }
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::teardown()
+{
+    LOG_PRINT << "ciao " << name();
+}
+
+/////////////////////////////////////////////////////////////////
+
+void ParticleSample::update_property(const Property::ConstPtr &theProperty)
+{
+    ViewerApp::update_property(theProperty);
+
+    if(theProperty == m_num_particles)
+    {
+        m_needs_init = true;
+    }
+    else if(theProperty == m_point_size)
+    {
+        m_particle_mesh->material()->set_point_size(*m_point_size);
+    }
+    else if(theProperty == m_point_color)
+    {
+        m_particle_mesh->material()->set_diffuse(*m_point_color);
+    }
+    else if(theProperty == m_start_velocity_min ||
+            theProperty == m_start_velocity_max)
+    {
+        m_needs_init = true;
+    }
+}
---
+++ opencl_particles/ParticleSample.hpp
@@ -0,0 +1,74 @@
+// __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+//
+// Copyright (C) 2012-2016, Fabian Schmidt <crocdialer@googlemail.com>
+//
+// It is distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt
+// __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+
+//  ParticleSample.hpp
+//
+//  Created by Fabian on 29/01/14.
+
+#pragma once
+
+#include "app/ViewerApp.hpp"
+
+// module headers
+#include "opencl/ParticleSystem.hpp"
+
+namespace kinski
+{
+    class ParticleSample : public ViewerApp
+    {
+    private:
+
+        Property_<string>::Ptr m_texture_path = Property_<string>::create("texture path", "");
+        RangedProperty<int>::Ptr m_num_particles = RangedProperty<int>::create("num particles", 100000, 1, 10000000);
+        RangedProperty<float>::Ptr m_point_size = RangedProperty<float>::create("point size", 3.f, 1.f, 64.f);
+        Property_<gl::Color>::Ptr m_point_color = Property_<gl::Color>::create("point color", gl::COLOR_WHITE);
+
+        Property_<gl::vec3>::Ptr
+        m_start_velocity_min = Property_<gl::vec3>::create("start velocity min"),
+        m_start_velocity_max = Property_<gl::vec3>::create("start velocity max");
+
+        RangedProperty<float>::Ptr
+        m_lifetime_min = RangedProperty<float>::create("lifetime min", 3.f, 0.f, 100.f),
+        m_lifetime_max = RangedProperty<float>::create("lifetime max", 3.f, 0.f, 100.f);
+
+        gl::ParticleSystem m_particle_system;
+        gl::MeshPtr m_particle_mesh;
+
+        void init_particles(uint32_t the_num);
+
+        bool m_needs_init = true;
+
+    public:
+        ParticleSample(int argc = 0, char *argv[] = nullptr):ViewerApp(argc, argv){};
+        void setup() override;
+        void update(float timeDelta) override;
+        void draw() override;
+        void resize(int w ,int h) override;
+        void key_press(const KeyEvent &e) override;
+        void key_release(const KeyEvent &e) override;
+        void mouse_press(const MouseEvent &e) override;
+        void mouse_release(const MouseEvent &e) override;
+        void mouse_move(const MouseEvent &e) override;
+        void mouse_drag(const MouseEvent &e) override;
+        void mouse_wheel(const MouseEvent &e) override;
+        void touch_begin(const MouseEvent &e, const std::set<const Touch*> &the_touches) override;
+        void touch_end(const MouseEvent &e, const std::set<const Touch*> &the_touches) override;
+        void touch_move(const MouseEvent &e, const std::set<const Touch*> &the_touches) override;
+        void file_drop(const MouseEvent &e, const std::vector<std::string> &files) override;
+        void teardown() override;
+        void update_property(const Property::ConstPtr &theProperty) override;
+    };
+}// namespace kinski
+
+int main(int argc, char *argv[])
+{
+    auto theApp = std::make_shared<kinski::ParticleSample>(argc, argv);
+    LOG_INFO<<"local ip: " << kinski::net::local_ip();
+    return theApp->run();
+}
