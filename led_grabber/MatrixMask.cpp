8a329edc221ad58f3fa71c94163824f5f1fcf6ec
crocdialer <crocdialer@gmail.com>
2018-09-18 18:38:14 +0200

    temp workaround for ES2 compat

--- led_grabber/MatrixMask.cpp
+++ led_grabber/MatrixMask.cpp
@@ -37,19 +37,36 @@ namespace kinski{namespace gl{
         }
         
         // generate image
-        uint8_t pixel[m_grid_cells.size()];
+        uint8_t pixels[m_grid_cells.size()];
         for(uint32_t i = 0; i < m_grid_cells.size(); ++i)
         {
-            pixel[i] = m_grid_cells[i].life_time > 0.f ? 255 : 0;
+            pixels[i] = m_grid_cells[i].life_time > 0.f ? 255 : 0;
         }
         
         // generate texture
 #if defined(KINSKI_GLES)
-        uint32_t format = GL_LUMINANCE;
+        GLint format = GL_LUMINANCE_ALPHA;
+
+        // create data
+        size_t num_bytes = m_size.x * m_size.y * 2;
+        auto luminance_alpha_data = std::unique_ptr<uint8_t>(new uint8_t[num_bytes]);
+        uint8_t *src_ptr = static_cast<uint8_t*>(pixels);
+        uint8_t *out_ptr = luminance_alpha_data.get(), *data_end = luminance_alpha_data.get() + num_bytes;
+
+        for (; out_ptr < data_end; out_ptr += 2, ++src_ptr)
+        {
+            out_ptr[0] = *src_ptr;
+            out_ptr[1] = 255;
+        }
+
+        // create a new texture object for our glyphs
+        gl::Texture::Format fmt;
+        fmt.internal_format = tex_format;
+        m_texture = gl::Texture(luminance_alpha_data.get(), tex_format, width, height, fmt);
 #else
         uint32_t format = GL_RED;
+        m_texture.update(pixels, format, m_size.x, m_size.y);
 #endif
-        m_texture.update(pixel, format, m_size.x, m_size.y);
         m_texture.set_mag_filter(GL_NEAREST);
     }
     
